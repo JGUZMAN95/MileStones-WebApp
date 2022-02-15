32e589db2b624d3735cb03f38e10cebccce5200d
anthonyzhang1 <anthony.zhang56@gmail.com>
2022-02-15 15:49:01 -0800

    fixed miro's page

--- applications/client/src/pages/about/AboutMiroslav.js
+++ applications/client/src/pages/about/AboutMiroslav.js
@@ -1,26 +1,16 @@
 import React from 'react';
-<<<<<<< HEAD
 import miroslavPic from '../../images/pictureOfMiroslav.jpg'
-=======
-import miroslavPic from '../../images/default about picture.png'
->>>>>>> 858f44c09054e9afc0084a1dce03f43efe02b3fa
 
 const AboutMiroslav = () => {
     return (
         <div style={{ padding: "0 1% 0 1%" }}>
             <img src={miroslavPic} width="250px" height="250px" alt="default"/>
 
-<<<<<<< HEAD
             <h1>Miroslav Stavrev</h1>
             <h2>Role: Scrum Master</h2>
             <p>Hello, my name is Miroslav Stavrev and I'm currently a senior at SFSU.<br/>
             My main hobby is gaming of any kind and I also enjoy reading, anime, and analysis.
             </p>
-=======
-            <h1>[Name]</h1>
-            <h2>Role: [Role]</h2>
-            <p>[Description]</p>
->>>>>>> 858f44c09054e9afc0084a1dce03f43efe02b3fa
         </div>
     );
 }
