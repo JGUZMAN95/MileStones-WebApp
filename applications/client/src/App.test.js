383139ed30265400ebe0188b4f4a742bf6e74f5d
anthonyzhang1 <anthony.zhang56@gmail.com>
2022-02-15 03:07:17 -0800

    Added a home page and some links.

--- applications/client/src/App.test.js
+++ applications/client/src/App.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// "Learn React" is long gone but lets keep it here in case we need
+// an example of how test() works.
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
