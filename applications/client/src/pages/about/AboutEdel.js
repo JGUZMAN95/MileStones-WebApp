c40a7d20bdee66ec8a0cabd11624bede48da34dd
EJ Cenario <65520225+kurtina09@users.noreply.github.com>
2022-02-15 20:03:12 -0800

    updating my about me

--- applications/client/src/pages/about/AboutEdel.js
+++ applications/client/src/pages/about/AboutEdel.js
@@ -1,18 +1,49 @@
 import React from 'react';
-import edelPic from '../../images/default about picture.png'
+import ProfileUi from 'react-profile-card';
+import edelPic from '../../images/pictureOfEdel.jpg'
 
 const AboutEdel = () => {
-    return (
-        <div style={{ padding: "0 1% 0 1%" }}>
-            <img src={edelPic} width="250px" height="250px" alt="default"/>
+    return ( 
+        // <div style = {
+        //     { padding: "0 1% 0 1%" }
+        // }>
 
-            <h1>Edel Jhon Cenario</h1>
-            <h2>Role: Front End Lead</h2>
-            <p>
-              Hi, I'm Edel, but you can call me Ejay. <br/>
-              I play guitar when I'm bored. <br/>
-              I like bowling, hanging out with friends, and eating out.
-            </p>
+        // <
+        // img src = { edelPic }
+        // width = "250px"
+        // height = "250px"
+        // alt = "default" 
+        // />
+
+        // <h1> Edel Jhon Cenario </h1> 
+        // <h2> Role: Front End Lead </h2> 
+
+        // <p>
+        // Hi, I 'm Edel, but you can call me Ejay. <br/>
+        // I play guitar when I 'm bored. <br/>
+        // I like bowling, hanging out with friends, and eating out. 
+        // </p> 
+        
+        // </div>
+
+        <div style = {
+            { marginLeft: 800}
+        }> 
+        <ProfileUi 
+            imgUrl={edelPic} 
+            name='EJ Cenario' 
+            designation='Front End Leader' 
+            />
+
+
+        <p style = {
+            {textAlign: "center",
+            marginLeft: -845}
+        }>
+        Hi, I 'm Edel, but you can call me Ejay. <br/>
+        I play guitar when I 'm bored. <br/>
+        I like bowling, hanging out with friends, and eating out. 
+        </p> 
         </div>
     );
 }
