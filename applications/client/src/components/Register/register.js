890c2fab9e223118942d4ea53ec8d052149d7bc0
EJ Cenario <kurtina09@yahoo.com>
2022-04-21 13:57:05 -0700

    adding login:

--- applications/client/src/components/Register/register.js
+++ applications/client/src/components/Register/register.js
@@ -1,46 +1,74 @@
 import React from "react"
 import "./register.css"
 
-export default function LogIn() {
+export default function CreateAccount() {
   return (
-    <div className="log-in flex-col-hstart-vstart clip-contents">
-      <div className="background flex-row-vstart-hstart">
-        <div className="rectangle-3" />
+    <div className="create-account flex-col-hstart-vstart clip-contents">
+      <div className="group-17">
+        <div className="group-3"></div>
       </div>
       <img
-        src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/imw3m18q89-517%3A4?alt=media&token=5f11bb1f-f053-4fdd-b6ad-7a69b4c74a37"
+        src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/1yminuy1bia-511%3A79?alt=media&token=6cb27ac4-a366-4407-84ec-c09289c48997"
         alt="Not Found"
-        className="drum-21"
+        className="rectangle-8"
       />
-      <p className="txt-957">Welcome Back</p>
-      <div className="group-075 flex-row">
-        <p className="txt-362">Password</p>
+      <p className="txt-209">OR</p>
+      <p className="txt-9109">Create Account</p>
+      <div className="sign-up-with-google flex-row-vstart-hstart">
         <img
-          src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/imw3m18q89-517%3A21?alt=media&token=320d3142-ce1b-4b28-ab13-6b2a2a936cc8"
+          src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/1yminuy1bia-511%3A67?alt=media&token=6ac63ca5-9931-443e-974c-50ab3f9993cd"
           alt="Not Found"
-          className="visibility-23"
+          className="search-1"
+        />
+        <p className="txt-764">Sign up with Google </p>
+      </div>
+      <div className="sign-up-with-facebook flex-row-vstart-hstart">
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/1yminuy1bia-511%3A70?alt=media&token=f22a5d51-ca4d-4a71-bb1e-a13970426a82"
+          alt="Not Found"
+          className="facebook-1"
+        />
+        <p className="txt-3610">Sign up with Facebook </p>
+      </div>
+      <p className="txt-269">Are you a Parent or Provider?</p>
+      <div className="frame-3 flex-row-vstart-hstart">
+        <p className="txt-348">Full Name</p>
+      </div>
+      <div className="frame-5 flex-row-vstart-hstart">
+        <p className="txt-348">Email Address</p>
+      </div>
+      <div className="frame-4 flex-row-vstart-hstart">
+        <p className="txt-482">Password</p>
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/1yminuy1bia-514%3A72?alt=media&token=5cfa9cfd-75c2-4322-bdc8-3a4d779f84d9"
+          alt="Not Found"
+          className="visibility-22"
         />
       </div>
-      <div className="group-467">
-        <p className="txt-8109">Email Address</p>
-      </div>
-      <p className="txt-195">Remember me</p>
-      <div className="toggle flex-col-hstart-vstart">
-        <div className="group-694">
-          <div className="ellipse-3" />
+      <p className="txt-769">Sign Up</p>
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/1yminuy1bia-511%3A99?alt=media&token=3cd2b7a6-6cab-43c0-b79f-209ceb094b22"
+        alt="Not Found"
+        className="basketball-1"
+      />
+      <p className="txt-335">Already have an account?</p>
+      <p className="txt-721">Log In here</p>
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/1yminuy1bia-518%3A7?alt=media&token=de3dbb2b-dd94-4dbf-b98d-bbc26df26baf"
+        alt="Not Found"
+        className="playtime-11"
+      />
+      <div className="signup-parent-provider flex-col-hstart-vstart">
+        <p className="txt-160">Parent</p>
+        <div className="group-1016 flex-row">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/1yminuy1bia-I542%3A202%3B542%3A182?alt=media&token=3773bb17-5ad8-4e8a-b591-908e07f89d34"
+            alt="Not Found"
+            className="rectangle-11"
+          />
+          <p className="txt-525">Provider</p>
         </div>
       </div>
-      <p className="txt-232">Forgot Password</p>
-      <div className="frame-14 flex-row-vstart-hstart">
-        <p className="txt-196 flex-hcenter">Log In</p>
-      </div>
-      <img
-        src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/imw3m18q89-518%3A3?alt=media&token=f3d22b15-4785-44d2-8acd-180e2b50a73b"
-        alt="Not Found"
-        className="teacher-21"
-      />
-      <p className="txt-5107">Don’t have an account yet?</p>
-      <p className="txt-192">Sign Up here!</p>
     </div>
   )
 }
--- applications/client/src/components/login/login.js
+++ applications/client/src/components/login/login.js
@@ -1,25 +1,46 @@
-import React from 'react';
-import './login.css'
+import React from "react"
+import "./login.css"
 
-export default function Login() {
-    return(
-        <div className={"Login"}>
-            <p class = "title" align ="center">SIGN IN</p>
-            <form class = "form">
-            <label>
-                <input class = "email" type="email"
-                align = "center" placeholder= "Email"/>
-            </label>
-            <label>
-                <input className="password" type="password"
-                       align="center" placeholder="Password"/>
-            </label>
-            <div>
-                <a className="submit" align="center">Sign In</a>
-                <p class="link" align = "center">
-                    <a href="#"/>Don't Have An Account?</p>
-            </div>
-        </form>
+export default function LogIn() {
+  return (
+    <div className="log-in flex-col-hstart-vstart clip-contents">
+      <div className="background flex-row-vstart-hstart">
+        <div className="rectangle-3" />
+      </div>
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/imw3m18q89-517%3A4?alt=media&token=5f11bb1f-f053-4fdd-b6ad-7a69b4c74a37"
+        alt="Not Found"
+        className="drum-21"
+      />
+      <p className="txt-957">Welcome Back</p>
+      <div className="group-075 flex-row">
+        <p className="txt-362">Password</p>
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/imw3m18q89-517%3A21?alt=media&token=320d3142-ce1b-4b28-ab13-6b2a2a936cc8"
+          alt="Not Found"
+          className="visibility-23"
+        />
+      </div>
+      <div className="group-467">
+        <p className="txt-8109">Email Address</p>
+      </div>
+      <p className="txt-195">Remember me</p>
+      <div className="toggle flex-col-hstart-vstart">
+        <div className="group-694">
+          <div className="ellipse-3" />
         </div>
-    )
+      </div>
+      <p className="txt-232">Forgot Password</p>
+      <div className="frame-14 flex-row-vstart-hstart">
+        <p className="txt-196 flex-hcenter">Log In</p>
+      </div>
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/imw3m18q89-518%3A3?alt=media&token=f3d22b15-4785-44d2-8acd-180e2b50a73b"
+        alt="Not Found"
+        className="teacher-21"
+      />
+      <p className="txt-5107">Don’t have an account yet?</p>
+      <p className="txt-192">Sign Up here!</p>
+    </div>
+  )
 }
