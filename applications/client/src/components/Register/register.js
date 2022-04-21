e163016bebdb5ce229b49bb9c3f513638b226c0d
EJ Cenario <kurtina09@yahoo.com>
2022-04-21 12:53:30 -0700

    Added Register page

--- applications/client/src/components/Register/register.js
+++ applications/client/src/components/Register/register.js
@@ -1,35 +1,46 @@
-import React from 'react';
-import './register.css'
+import React from "react"
+import "./register.css"
 
-export default function register() {
-    return(
-        <div className={"register"}>
-            <p class = "title" align ="center">SIGN UP</p>
-            <form class = "form">
-                <label>
-                    <input class = "email" type="email"
-                           align = "center" placeholder= "Email"/>
-                </label>
-                <label>
-                    <input className="password" type="password"
-                           align="center" placeholder="Password"/>
-                </label>
-                <p class = "text" align = "left"> Are You A...
-                <input type="checkbox" class="parent" name="parent" value="parent"/>
-                    <label htmlFor="parent"> Parent</label>
-                    <input type="checkbox" className="provider" name="provider" value="provider"/>
-                    <label htmlFor="provider"> Provider</label></p>
-
-                <p class = "text" align = "left"> Check The Box If You Agree To The Terms Of Services
-
-                <input type="checkbox" align= "center" className="tos" name="tos" value="tos"/>
-                <label htmlFor="tos" > </label> </p>
-                <div>
-                    <a className="submit" align="center">Sign Up</a>
-                    <p class="li" align = "center">
-                        <a href="#"/>Already Have an Account?</p>
-                </div>
-            </form>
+export default function LogIn() {
+  return (
+    <div className="log-in flex-col-hstart-vstart clip-contents">
+      <div className="background flex-row-vstart-hstart">
+        <div className="rectangle-3" />
+      </div>
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/imw3m18q89-517%3A4?alt=media&token=5f11bb1f-f053-4fdd-b6ad-7a69b4c74a37"
+        alt="Not Found"
+        className="drum-21"
+      />
+      <p className="txt-957">Welcome Back</p>
+      <div className="group-075 flex-row">
+        <p className="txt-362">Password</p>
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/imw3m18q89-517%3A21?alt=media&token=320d3142-ce1b-4b28-ab13-6b2a2a936cc8"
+          alt="Not Found"
+          className="visibility-23"
+        />
+      </div>
+      <div className="group-467">
+        <p className="txt-8109">Email Address</p>
+      </div>
+      <p className="txt-195">Remember me</p>
+      <div className="toggle flex-col-hstart-vstart">
+        <div className="group-694">
+          <div className="ellipse-3" />
         </div>
-    )
+      </div>
+      <p className="txt-232">Forgot Password</p>
+      <div className="frame-14 flex-row-vstart-hstart">
+        <p className="txt-196 flex-hcenter">Log In</p>
+      </div>
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/imw3m18q89-518%3A3?alt=media&token=f3d22b15-4785-44d2-8acd-180e2b50a73b"
+        alt="Not Found"
+        className="teacher-21"
+      />
+      <p className="txt-5107">Don’t have an account yet?</p>
+      <p className="txt-192">Sign Up here!</p>
+    </div>
+  )
 }
