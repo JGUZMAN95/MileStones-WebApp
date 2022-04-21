5efb26ab01b9c395163420dbba166d1c24595383
Jocelyn Guzman <jocey.g.95@gmail.com>
2022-04-21 13:59:54 -0700

    proxy error

--- applications/server/index.js
+++ applications/server/index.js
@@ -2,13 +2,31 @@ const express = require("express");
 const port = process.env.PORT || 3001;
 const app = express();
 
-const mongoose = require("mongoose");
-const mongoDB = 'mongodb://127.0.0.1/milestones_db';
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+const { MongoClient, ServerApiVersion } = require('mongodb');
+async function main() {
+    const uri = "mongodb+srv://MilestonesAdmin:CSC-648Team01@milestones.xaphw.mongodb.net/test";
+    const client = new MongoClient(uri);
+    try {
+        //connecting to database
+        await client.connect();
+        //listing databases
+        await listDatabases(client);
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+main().catch(console.error);
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error.'));
+async function listDatabases(client){
+    const dbList = await client.db().admin().listDatabases();
 
+    console.log("Databases:");
+    dbList.databases.forEach(db => {
+        console.log(db.name);
+    })
+}
 app.get("/api", (req, res) => {
     res.json({message: "Successfully connected Express with React."});
 });
