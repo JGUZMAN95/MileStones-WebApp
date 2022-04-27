546efbdbb4107213c18c8357cdb5bc5419f589a7
Hira Afzal <afzal_99hira@yahoo.com>
2022-04-27 00:53:27 -0700

    changes to child profile js and css

--- applications/client/src/components/profile/childprofile.js
+++ applications/client/src/components/profile/childprofile.js
@@ -1,42 +1,35 @@
 import React from "react"
 import "./childprofile.css"
+import icon from '../../images/monster.png'
 
 export default function ViewChildProfiles() {
   return (
-    <div className="view-child-profiles flex-col-hstart-vstart clip-contents">
-      <div className="group-095 flex-row-vcenter">
-        <img
-          src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/9vvvfo727yg-516%3A237?alt=media&token=e5d2d789-236c-4c38-abc2-0f17e4de7141"
-          alt="Not Found"
-          className="group-16"
-        />
-        <div className="group-898 flex-col">
-          <div className="childstoggle flex-row-vend-hstart">
-            <div className="frame-16 flex-row-vstart-hstart">
-              <p className="txt-7710 flex-hcenter">Education</p>
+      <div class={"float-container"} className={"profile"}>
+        <div className={"profile-box"}>
+          <img src = {icon}
+            className={"icon"}/>
+          <label className={'profile-text'}>
+            <h1>Name: </h1>
+            <h1>Age: </h1>
+         </label>
+        </div>
+        <div className={"rec-box"}>
+            <div class={"float-container"} className={'button'}>
+              <div className={'education-button'}>
+                <button className={'edu-bttn'} onClick={''}>
+                  Education
+                </button>
+              </div>
+              <div className={'activities-button'}>
+                <button className={'act-bttn'} onClick={''}>
+                  Activites
+                </button>
+              </div>
             </div>
-            <div className="line-4" />
-            <div className="frame-17 flex-row-vstart-hstart">
-              <p className="txt-7710 flex-hcenter">Activities</p>
-            </div>
-          </div>
-          <img
-            src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/9vvvfo727yg-525%3A138?alt=media&token=8c40da37-5a07-493b-bab7-58de8c5cc25f"
-            alt="Not Found"
-            className="rectangle-40"
-          />
         </div>
       </div>
-      <div className="profileimageandname flex-col-hcenter-vstart">
-        <img
-          src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/9vvvfo727yg-543%3A148?alt=media&token=2319ac26-4f2a-4f42-8c3e-f67db3e9d9eb"
-          alt="Not Found"
-          className="profileimage"
-        />
-        <div className="profilesname flex-col-hstart-vstart">
-          <p className="txt-764 flex-hcenter">Name</p>
-        </div>
-      </div>
-    </div>
+
+
+
   )
 }
