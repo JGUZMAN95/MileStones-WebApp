5d2ff949d8c4753151cd4e61987c1a11657909dc
EJ Cenario <kurtina09@yahoo.com>
2022-05-05 13:58:06 -0700

    revised viewpost

--- applications/client/src/pages/viewPosts.js
+++ applications/client/src/pages/viewPosts.js
@@ -1,78 +1,62 @@
 import React, { useEffect, useState } from "react";
-//import { Link } from "react-router-dom";
-//import "./viewPosts.css"
-import './Cards.css';
-import CardItem from './CardItem';
+import { Link } from "react-router-dom";
+import "./viewPosts.css"
+import { Card } from "react-bootstrap"
 
-const Providers = (props) => (
-          <CardItem
-            src='/babysit.jpg'
-            text={props.provider.name}
-            label={props.provider.service}
-            path='/services'
-          />
 
-        
-);
-    export default function SearchViewPosts() {
-        const [providers, setProviders] = useState([]);
+const Providers = (props) => [
+    <Card style={{ width: "18rem" }} key={props._id} className="box">
 
-        // This method fetches the records from the database.
-        useEffect(() => {
-            async function getProviders() {
-                const response = await fetch(`http://localhost:3001/post/`);
-                if (!response.ok) {
-                    const message = `An error occurred: ${response.statusText}`;
-                    window.alert(message);
-                    return;
-                }
-                const providers = await response.json();
-                setProviders(providers);
+            <Card.Img variant="top" src="holder.js/100px180"  />
+            <Card.Body>
+              <Card.Title>{props.provider.name}</Card.Title>
+              <Card.Text>{props.provider.rate}</Card.Text>
+            </Card.Body>
+            <Card.Footer>{props.provider.service}</Card.Footer>
+          </Card>
+];
+
+
+function SearchViewPosts() {
+    //----------------------------------------------------------------
+    const [providers, setProviders] = useState([]);
+
+    // This method fetches the records from the database.
+    useEffect(() => {
+        async function getProviders() {
+            const response = await fetch(`http://localhost:3001/post/`);
+            if (!response.ok) {
+                const message = `An error occurred: ${response.statusText}`;
+                window.alert(message);
+                return;
             }
-
-            getProviders();
-            return;
-        }, [providers.length]);
-
-        // This method will map out the records on the table
-        function providerList() {
-            return providers.map((provider) => {
-                return (
-                    <Providers
-                        provider={provider}
-                        key={provider._id}
-                    />
-                );
-            });
+            const providers = await response.json();
+            setProviders(providers);
         }
 
-        return (
-            <div className='cards'>
-            <div className='cards__container'>
-            <div className='cards__wrapper'>
-            <ul className='cards__items'>
-            {providerList()}
-            </ul>
-            </div>
-            </div>
-            </div>
+        getProviders();
+        return;
+    }, [providers.length]);
 
-            /*
-            <div>
-                <h3>Record List</h3>
-                <table className="table table-striped" style={{ marginTop: 20 }}>
-                    <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Rate</th>
-                        <th>Bio</th>
-                        <th>Expirence</th>
-                        <th>Services</th>
-                    </tr>
-                    </thead>
-                    <tbody>{providerList()}</tbody>
-                </table>
-            </div>
-        */
-        );
+    // This method will map out the records on the table
+    function providerList() {
+        return providers.map((provider) => {
+            return (
+                <Providers
+                    provider={provider}
+                    key={provider._id}
+                />
+            );
+        });
     }
+    //------------------------------------------------
+
+    return (
+        <div className="grid">{providerList()}</div>
+    );
+    
+    };
+
+
+
+export default SearchViewPosts
