6a3f76e57b9e663178e644d499e6749d12a7d141
EJ Cenario <kurtina09@yahoo.com>
2022-05-18 16:19:25 -0700

    fixing viewposts

--- applications/client/src/pages/viewPosts.js
+++ applications/client/src/pages/viewPosts.js
@@ -1,76 +1,70 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Component } from "react";
+import ReactSearchBox from "react-search-box";
 import "./viewPosts.css"
 import Card from 'react-bootstrap/Card'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Col from 'react-bootstrap/Col'
-import Row from 'react-bootstrap/Row'
-import { CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
+import { CardGroup } from 'reactstrap';
 
 
 const Providers = (props) => [
-  <div class="container">
-
-  {Array.from({ length: 1 }).map((_, idx) => (
-    <Col md = {3}>
-      <Card style={{marginTop: 50, marginBottom: 50}}>
-        <Card.Img variant="top" width="100%" src='/babysit.jpg'/>
-        <Card.Body>
-          <Card.Title>{props.provider.name}</Card.Title>
-          <Card.Text>
+  <CardGroup>
+    <Card border = "dark" style={{width: '18rem', marginBottom: '2rem', marginTop: '2rem', marginLeft: '2rem', marginRight: '2rem'}} className="text-center">
+      <Card.Img variant="top" width="100%" src='/babysit.jpg' />
+      <Card.Body>
+        <Card.Title>{props.provider.name}</Card.Title>
+        <Card.Text>
           {props.provider.rate}
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>{props.provider.service}</Card.Footer>
-      </Card>
-    </Col>
-  ))}
-
-  </div>
-
-
-
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer>{props.provider.service}</Card.Footer>
+    </Card>
+    </CardGroup>
 ];
 
 
 function SearchViewPosts() {
-    //----------------------------------------------------------------
-    const [providers, setProviders] = useState([]);
+  //----------------------------------------------------------------
+  const [providers, setProviders] = useState([]);
 
-    // This method fetches the records from the database.
-    useEffect(() => {
-        async function getProviders() {
-            const response = await fetch(`http://localhost:3001/post/`);
-            if (!response.ok) {
-                const message = `An error occurred: ${response.statusText}`;
-                window.alert(message);
-                return;
-            }
-            const providers = await response.json();
-            setProviders(providers);
-        }
-
-        getProviders();
+  // This method fetches the records from the database.
+  useEffect(() => {
+    async function getProviders() {
+      const response = await fetch(`http://localhost:3001/post/`);
+      if (!response.ok) {
+        const message = `An error occurred: ${response.statusText}`;
+        window.alert(message);
         return;
-    }, [providers.length]);
-
-    // This method will map out the records on the table
-    function providerList() {
-        return providers.map((provider) => {
-            return (
-                <Providers
-                    provider={provider}
-                    key={provider._id}
-                />
-            );
-        });
+      }
+      const providers = await response.json();
+      setProviders(providers);
     }
-    //------------------------------------------------
 
-    return (
-        <div className="grid">{providerList()}</div>
-    )
+    getProviders();
+    return;
+  }, [providers.length]);
+
+  // This method will map out the records on the table
+  function providerList() {
+    return providers.map((provider) => {
+      return (
+        <Providers
+          provider={provider}
+          key={provider._id}
+        />
+      );
+    });
+  }
+  //------------------------------------------------
+
+  return (
     
-    }
+
+    <CardGroup>
+      {providerList()}
+    </CardGroup>
+  )
+
+}
 
 
 
