f5fb69a5772d885bd0b3c78ee968fd89dbdaad4f
EJ Cenario <kurtina09@yahoo.com>
2022-05-18 19:34:19 -0700

    Commenting out Login

--- applications/client/src/components/Navbar/index.js
+++ applications/client/src/components/Navbar/index.js
@@ -12,28 +12,28 @@ const Navbar = () => {
   return (
     <>
       <Nav>
-        <NavLink to='/'> 
-        {/* Website Icon -> Routed back to Home page when clicked */}
-          { <img src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/000000/external-milestone-achievements-flaticons-lineal-color-flat-icons.png"/> }
+        <NavLink to='/'>
+          {/* Website Icon -> Routed back to Home page when clicked */}
+          {<img src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/000000/external-milestone-achievements-flaticons-lineal-color-flat-icons.png" />}
         </NavLink>
         <Bars />
         <NavMenu>
-        {/* Home button in NavBar */}
-        <NavLink to='/' activestyle = "true">
-          Home
-        </NavLink>
-        {/* About to page to Meet the team */}
-        <NavLink to='/about' activestyle  = "true">
+          {/* Home button in NavBar */}
+          <NavLink to='/' activestyle="true">
+            Home
+          </NavLink>
+          {/* About to page to Meet the team */}
+          <NavLink to='/about' activestyle="true">
             About
           </NavLink>
-        {/* Create post page -> Only available to Logged in Users*/}
-          <NavLink to='/createPost' activestyle  = "true">
-            Create Posts 
-        </NavLink>
-        {/* View post page -> Only available to logged in Users */}
-        <NavLink to='/viewPosts' activestyle  = "true">
-          View Posts
-        </NavLink>
+          {/* Create post page -> Only available to Logged in Users*/}
+          <NavLink to='/createPost' activestyle="true">
+            Create Posts
+          </NavLink>
+          {/* View post page -> Only available to logged in Users */}
+          <NavLink to='/viewPosts' activestyle="true">
+            View Posts
+          </NavLink>
         </NavMenu>
         <NavBtn>
           {/* Sign up -> Only available to logged out users. 
--- applications/client/src/components/login/login.js
+++ applications/client/src/components/login/login.js
@@ -5,45 +5,45 @@ import abcdTeacher from '../../images/teacher (2) 1.png'
 import { useParams, useNavigate, useLocation } from "react-router-dom"
 export default function LogIn() {
     // allows us to be able to call for url to pass as json to server
-    const {login} = useParams();
-        //need to be able to get user input from form and update constant changes
-        const [user, setUser] = useState( {
-            email:"",
-            password:"",
-        });
+    const { login } = useParams();
+    //need to be able to get user input from form and update constant changes
+    const [user, setUser] = useState({
+        email: "",
+        password: "",
+    });
 
-        const navigate = useNavigate();
-        //updating the constanst change that the user is doing through out the form
-        //by setting the information that is in being passed over
-        function handleChange(value){
-           return setUser((prev)=>{
-               return{...prev, ...value}
-           })
+    const navigate = useNavigate();
+    //updating the constanst change that the user is doing through out the form
+    //by setting the information that is in being passed over
+    function handleChange(value) {
+        return setUser((prev) => {
+            return { ...prev, ...value }
+        })
     }
     //once user clicks on the submit button then we can get the url to be able
 
-    function onSubmit(){
+    function onSubmit() {
         // calling the server to be able to check if login info exsits
         const response = fetch(`http://localhost:3001/login/`, {
-                 method: "POST",
-                 headers: {
-                     "Content-Type": "application/json",
-                    },
-                    //passing in the url as a json since we want a return after a post
-                    //we need to pass as json
-                    body: JSON.stringify(login),
-                 });
-                 //Once message from server is captured we will check what response
-                 //we were given depending on that we stay in login or we go to signed in home page
-                 if(!response){
-                    navigate("/login")
-                 }else{
-                     navigate("/")
-                 }
-    }     
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            //passing in the url as a json since we want a return after a post
+            //we need to pass as json
+            body: JSON.stringify(login),
+        });
+        //Once message from server is captured we will check what response
+        //we were given depending on that we stay in login or we go to signed in home page
+        if (!response) {
+            navigate("/login")
+        } else {
+            navigate("/")
+        }
+    }
 
 
-//e.preventDefault()
+    //e.preventDefault()
 
     return (
         <div className="log-in flex-col-hstart-vstart clip-contents">
@@ -51,42 +51,41 @@ export default function LogIn() {
                 <div className="rectangle-3">
                     <p className="txt-957">Welcome Back</p>
 
-                    <form className = "login" onSubmit={onSubmit}>
-
+                    <form className="login" onSubmit={onSubmit}>
+                        {/* Field to get email address from user */}
                         <input className="group-467 flex-row-vstart-hstart"
-                               type = "email"
-                               placeholder = "Email Address"
-                               value = {user.email}
-                               name="email"
-                               onChange={(e)=>handleChange({email: e.target.value})}></input>
+                            type="email"
+                            placeholder="Email Address"
+                            value={user.email}
+                            name="email"
+                            onChange={(e) => handleChange({ email: e.target.value })}></input>
+
+                        {/* Field to get password from user */}
                         <input className="group-075 flex-row-vstart-hstart"
-                               type = "password" 
-                               placeholder = "Password"
-                               value = {user.password}
-                               name="password"
-                               onChange={(e)=>handleChange({password: e.target.value})}></input>
+                            type="password"
+                            placeholder="Password"
+                            value={user.password}
+                            name="password"
+                            onChange={(e) => handleChange({ password: e.target.value })}></input>
 
-                        <input type="checkbox" className ="checkbox"></input>
+                        <input type="checkbox" className="checkbox"></input>
                         <label for="rmbme" className="txt-195">Remember me</label>
-                            
 
+                        {/* Submit Button */}
                         <input className="frame-14 flex-row-vstart-hstart"
-                               type = "submit"
-                               value = "Log In"
-                               //onClick={(e) => {login(e.preventDefault())}}
+                            type="submit"
+                            value="Log In"
+                        //onClick={(e) => {login(e.preventDefault())}}
                         ></input>
                     </form>
 
-                    {/* needs account recovery page */}
-                    <p className="txt-232">Forgot Password</p>
-
                     <img
-                        src= {abcdTeacher}
+                        src={abcdTeacher}
                         alt="Not Found"
                         className="teacher-21"
                     />
                     <p className="txt-5107">Don’t have an account yet?</p>
-                    <a className="txt-192" href = './register'>Sign Up here!</a>
+                    <a className="txt-192" href='./register'>Sign Up here!</a>
                 </div>
             </div>
             <img
@@ -94,7 +93,7 @@ export default function LogIn() {
                 alt="Not Found"
                 className="drum-21"
             />
-            
+
         </div>
     );
 }
